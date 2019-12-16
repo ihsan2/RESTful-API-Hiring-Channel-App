52cc3ecafca3bed4cd3fa686f2a12c5061321bcf
ihsan2 <nihsan727@gmail.com>
2019-12-16 23:45:15 +0700

    fix db connection

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 // import dependencies
-const express = require('express')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv')
-const logger = require('morgan')
-var cors = require('cors')
-const routerNav = require('./src/index')
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const logger = require("morgan");
+var cors = require("cors");
+const routerNav = require("./src/index");
 
 // use dependencies
-const app = express()
-app.use(bodyParser.json()) // parsing json
-app.use(bodyParser.urlencoded({ extended: true })) // parsing x-www-formencoded
-app.use(logger('dev'))
-app.use(cors())
-dotenv.config()
+const app = express();
+app.use(bodyParser.json()); // parsing json
+app.use(bodyParser.urlencoded({ extended: true })); // parsing x-www-formencoded
+app.use(logger("dev"));
+app.use(cors());
+dotenv.config();
 
 // middleware
-app.use('/api/v1', routerNav)
+app.use("/api/v1", routerNav);
 
-app.get('*', (req, res) => {
-  res.send('Error! 404 not Found.')
-})
+app.get("*", (req, res) => {
+  res.send("Error! 404 not Found.");
+});
 
 // listen to connection with callback function
-app.listen(process.env.PORT, function () {
-  console.log(`CORS-enabled web server listening on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, function() {
+  console.log(`CORS-enabled web server listening on port ${process.env.PORT}`);
+});
--- src/configs/db.js
+++ src/configs/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql')
-const dotenv = require('dotenv')
-dotenv.config()
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
 
 var conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PAST,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME
-})
+});
 conn.connect(err => {
   if (err) {
-    console.log(`Error \n ${err}`)
+    console.log(`Error \n ${err}`);
   } else {
-    console.log('Success Connect To Database')
+    console.log("Success Connect To Database");
   }
-})
+});
 
-module.exports = conn
+module.exports = conn;
