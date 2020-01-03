9d3fa7776c8aff2f170502b52d2bbea334ac22c0
ihsan2 <nihsan727@gmail.com>
2020-01-03 10:23:36 +0700

    add tes

--- src/routes/tes.js
+++ src/routes/tes.js
@@ -3,6 +3,6 @@ const Route = express.Router();
 // import controller
 const tes = require("../controllers/tes");
 
-Route.get("/", tes.getTes);
+Route.get("/", tes.getTes).post("/", tes.createTes);
 
 module.exports = Route;
