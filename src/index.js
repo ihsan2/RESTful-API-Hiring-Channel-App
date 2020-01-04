6eb189af3940f554d833f61a865e6991f9bfc401
ihsan2 <nihsan727@gmail.com>
2020-01-04 17:02:05 +0700

    fix routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const Route = express.Router()
+const express = require("express");
+const Route = express.Router();
 
 // import routes
-const engineers = require('./routes/engineers')
-const companies = require('./routes/companies')
-const auth = require('./routes/auth')
-const message = require('./routes/message')
+const engineers = require("./routes/engineers");
+const companies = require("./routes/companies");
+const auth = require("./routes/auth");
+const message = require("./routes/message");
+const tes = require("./routes/tes");
 
-Route
-  .use('/engineers', engineers)
-  .use('/companies', companies)
-  .use('/auth', auth)
-  .use('/message', message)
+Route.use("/engineers", engineers)
+  .use("/companies", companies)
+  .use("/auth", auth)
+  .use("/message", message)
+  .use("/tes", tes);
 
-module.exports = Route
+module.exports = Route;
