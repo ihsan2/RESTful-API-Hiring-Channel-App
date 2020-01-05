3ccce30175a3bc0844e255d9d0cf2fc0ab809b6b
ihsan2 <nihsan727@gmail.com>
2020-01-05 18:38:03 +0700

    fix message

--- src/routes/message.js
+++ src/routes/message.js
@@ -7,7 +7,7 @@ const message = require("../controllers/message");
 Route.post("/toEngineer", message.addMessageToEngineer)
   .post("/toCompany", message.addMessageToCompany)
   .get("/", message.getMessage)
-  .delete("/toCompany/:id_message", message.deleteMessagetoCompany);
+  .delete("/toCompany/:id_message", message.deleteMessagetoCompany)
   .delete("/toEngineer/:id_message", message.deleteMessagetoEngineer);
 
 module.exports = Route;
