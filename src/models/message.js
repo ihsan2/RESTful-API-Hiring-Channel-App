8f3b60fbd79c2764f795321025abd50e5f6ae443
ihsan2 <nihsan727@gmail.com>
2020-01-05 18:22:44 +0700

    fix message

--- src/models/message.js
+++ src/models/message.js
@@ -41,7 +41,7 @@ module.exports = {
   deleteMessage: id_message => {
     return new Promise((resolve, reject) => {
       db.query(
-        "DELETE from engineer WHERE id_message = ?",
+        "DELETE from message WHERE id_message = ?",
         id_message,
         (err, result) => {
           if (!err) {
