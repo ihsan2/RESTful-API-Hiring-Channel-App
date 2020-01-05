dc76d35c10305d7accae320c223533a1f05df0c4
ihsan2 <nihsan727@gmail.com>
2020-01-05 18:18:36 +0700

    fix message

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -5,7 +5,7 @@ module.exports = {
   addMessageToEngineer: (req, res) => {
     const { id_company, id_engineer, message } = req.body;
     const date_created = new Date();
-    const id_message = `${id_company}S${id_engineer}S${Date.now()}`;
+    const id_message = `${id_company}_${id_engineer}_${Date.now()}`;
     const data = { id_message, id_company, id_engineer, message, date_created };
     messageModel
       .addMessageToEngineer(data)
@@ -28,7 +28,7 @@ module.exports = {
   addMessageToCompany: (req, res) => {
     const { id_company, id_engineer, message } = req.body;
     const date_created = new Date();
-    const id_message = `${id_company}S${id_engineer}S${Date.now()}`;
+    const id_message = `${id_company}_${id_engineer}_${Date.now()}`;
     const data = { id_message, id_company, id_engineer, message, date_created };
     messageModel
       .addMessageToCompany(data)
